7c54595967ffed22e3787008ef454d990e7c7db7
Illia Kovalov <kovalev.illia2009@gmail.com>
2023-11-14 22:21:10 +0100

    171717

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,13 +10,33 @@ import css from './App.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
+  };
+
+  componentDidMount = () => {
+    const data = JSON.parse(localStorage.getItem('contact-list'));
+    if (data) {
+      this.setState({ contacts: [...data] });
+    } else {
+      this.setState({
+        contacts: [
+          { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+          { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+          { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+          { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+        ],
+      });
+    }
+  };
+
+  componentDidUpdate = prevState => {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(
+        'contact-list',
+        JSON.stringify(this.state.contacts)
+      );
+    }
   };
 
   isNameHas = name => {
